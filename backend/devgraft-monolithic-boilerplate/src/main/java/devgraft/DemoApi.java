096753fbebf385753f4cf807f092f766b219b6fa
PCloud <pcloud63514@gmail.com>
2022-11-26 16:31:48 +0900

    refactor: init

--- backend/devgraft-monolithic-boilerplate/src/main/java/devgraft/DemoApi.java
+++ backend/devgraft-monolithic-boilerplate/src/main/java/devgraft/DemoApi.java
@@ -1,13 +1,13 @@
 package devgraft;
 
-import devgraft.support.sentry.SentryMessenger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoApi {
+
     @GetMapping("home")
     public String home() {
-        return "Well Come Devgraft! / app-continuum-boilerplate";
+        return "Well Come Devgraft! / monolithic-boilerplate";
     }
 }
---
+++ backend/devgraft-monolithic-boilerplate/src/main/java/devgraft/DevgraftMonolithicBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package devgraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevgraftMonolithicBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevgraftMonolithicBoilerplateApplication.class, args);
+    }
+
+}
