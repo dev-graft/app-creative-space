710847e0a3798586431b919644c81c4e33dfba1c
PCloud <pcloud63514@gmail.com>
2022-11-26 16:20:51 +0900

    refactor: init

--- backend/devgraft-app-continuum-boilerplate/applications/app-demo/src/main/java/devgraft/DemoApi.java
+++ backend/devgraft-app-continuum-boilerplate/applications/app-demo/src/main/java/devgraft/DemoApi.java
@@ -6,14 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoApi {
-    @GetMapping("v1/home")
+    @GetMapping("home")
     public String home() {
-        SentryMessenger.send(new RuntimeException("센트리 테스트입니다.222"));
-        return "home_v1";
-    }
-
-    @GetMapping("v2/home")
-    public String homeV2() {
-        return "home_v2";
+        return "Well Come Devgraft! / app-continuum-boilderplate";
     }
 }
